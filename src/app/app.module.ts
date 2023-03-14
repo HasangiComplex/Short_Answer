43d04bf25654cf5a9cdd563ab7916970605fa8fb
hansika_112197 <hansika.piumali@axiatadigitallabs.com>
2023-03-14 13:20:31 +0530

    create footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PopupModalComponent } from './popup-modal/popup-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PopupModalComponent
   ],
   imports: [
     BrowserModule
